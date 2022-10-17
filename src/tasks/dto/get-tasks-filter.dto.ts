4c1e6cded81c80daf2014569362d9d693f446ea2
thanhhung270502 <thanhhung270502@gmail.com>
2022-10-17 10:37:18 +0700

    Validation: Filter and Search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { isString } from 'util';
 import { TaskStatus } from './../task.model';
 export class GetTasksFilterDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
     search?: string;
 }
