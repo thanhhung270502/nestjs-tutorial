8e53df1fb2c750dde526e9935a61dd64db1b8ae2
thanhhung270502 <thanhhung270502@gmail.com>
2022-10-19 15:45:50 +0700

    Bcrypt Password & Sign In

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,18 +20,42 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.usersRepository.create({ username, password });
-    
+    // Hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log('salt', salt);
+    console.log('hashedPassword', hashedPassword);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+
     try {
       await this.usersRepository.save(user);
     } catch (error) {
       if (error.code === '23505') {
         // duplicate username
         throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
       }
-      else {
-        throw new InternalServerErrorException()
-      }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({
+      where: {
+        username: username,
+      },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
     }
   }
 }
